f5b15bf901a725ee6d953bd24c6477304e639142
Haroon Ghawsi <ghawsiharoon@gmail.com>
2018-11-06 03:51:09 +0100

    Home component added

--- src/app/index.js
+++ src/app/index.js
@@ -3,18 +3,21 @@ import { render } from 'react-dom';
 
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+import { Projects } from "./components/Projects";
+import { Blog } from "./components/Blog";
+
 
 class App extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container" >
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
                         <Header/>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row1">
                     <div className="col-xs-10 col-xs-offset-1">
                         <Home/>
                     </div>
