65f298c5215bea54b890170688df15a3d1eea483
Haroon Ghawsi <ghawsiharoon@gmail.com>
2018-11-07 02:04:59 +0100

    update blog/projects pages

--- src/app/components/Blog.js
+++ src/app/components/Blog.js
@@ -7,6 +7,35 @@ export class Blog extends React.Component {
                 <div className="jumbotron">
                     <h5>Blog!</h5>
                 </div>
+                <div className="list-group">
+                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">Tutorial Heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Tutorial Content...</p>
+                        <small className="text-muted">References:....</small>
+                        <small className="text-muted">GitHub</small>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">Tutorial Heading</h5>
+                            <small className="text-muted">3 days ago</small>
+                        </div>
+                        <p className="mb-1">Tutorial Content...</p>
+                        <small className="text-muted">References:....</small>
+                        <small className="text-muted">GitHub</small>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">Tutorial Heading</h5>
+                            <small className="text-muted">3 days ago</small>
+                        </div>
+                        <p className="mb-1">Tutorial Content...</p>
+                        <small className="text-muted">References:....</small>
+                        <small className="text-muted">GitHub</small>
+                    </a>
+                </div>
             </div>
         );
     }
--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -7,6 +7,36 @@ export class Projects extends React.Component {
                 <div className="jumbotron">
                     <h5>Projects!</h5>
                 </div>
+                <div className="list-group">
+                <div className="card text-white bg-dark mb-3">
+                    <div className="card-header">Project Header</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Project Title</h5>
+                        <p className="card-text">Project Description</p>
+                    </div>
+                </div>
+                <div className="card text-white bg-dark mb-3">
+                    <div className="card-header">Project Header</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Project Title</h5>
+                        <p className="card-text">Project Description</p>
+                    </div>
+                </div>
+                <div className="card text-white bg-dark mb-3">
+                    <div className="card-header">Project Header</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Project Title</h5>
+                        <p className="card-text">Project Description</p>
+                    </div>
+                </div>
+                <div className="card text-white bg-dark mb-3">
+                    <div className="card-header">Project Header</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Project Title</h5>
+                        <p className="card-text">Project Description</p>
+                    </div>
+                </div>
+                </div>
             </div>
         );
     }
